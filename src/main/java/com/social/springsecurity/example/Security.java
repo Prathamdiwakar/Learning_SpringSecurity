98f8d40b38173cc2c00f82595cb3a374b4d47470
Prathamdiwakar <prathamdiwakar44@gmail.com>
2026-03-06 17:01:19 +0530

    Implementation of  SpringSecurityApplication

--- src/main/java/com/social/springsecurity/example/Security.java
+++ src/main/java/com/social/springsecurity/example/Security.java
@@ -1,14 +1,28 @@
 package com.social.springsecurity.example;
 
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableMethodSecurity
 @RestController
 public class Security {
 
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello World";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndPoint() {
+        return "Hello, User!";
+    }
+
+    @GetMapping("/admin")
+    public String adminEndPoint() {
+        return "Hello, Admin!";
     }
 }
